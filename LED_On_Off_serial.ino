f6e32e44acee98a63a0a9e78b17c9523fb20d682
Divyesh Hadvani <34337542+DivyeshHadvani@users.noreply.github.com>
2019-12-17 16:02:29 +0530

    Add files via upload

---
+++ LED_On_Off_serial.ino
@@ -0,0 +1,30 @@
+char serialData;
+int pin=8;
+
+
+void setup() 
+{
+  pinMode(pin, OUTPUT);
+  Serial.begin(9600);
+  digitalWrite( pin, HIGH);
+  // put your setup code here, to run once:
+
+}
+
+void loop() 
+{
+  if (Serial.available() >0)
+  {
+    serialData = Serial.read();
+    Serial.print(serialData);
+  }
+  if (serialData == '1') // if (serialData == 49)
+  {
+    digitalWrite( pin, HIGH);
+  }
+    else if (serialData == '0') // elseif (serialData == 48)
+  {
+    digitalWrite( pin, LOW);
+  }
+  // put your main code here, to run repeatedly:
+}
